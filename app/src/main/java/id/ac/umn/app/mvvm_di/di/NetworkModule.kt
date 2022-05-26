cad86c8a876bd1367cd0a0c0e8a6f0649dfe021a
Gerardo Jason <gerardoojason@gmail.com>
2022-05-26 12:20:01 +0700

    Dagger hilt commit 2

---
+++ app/src/main/java/id/ac/umn/app/mvvm_di/di/NetworkModule.kt
@@ -0,0 +1,59 @@
+package id.ac.umn.app.mvvm_di.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import id.ac.umn.app.mvvm_di.BuildConfig
+import id.ac.umn.app.mvvm_di.data.service.ApiHelper
+import id.ac.umn.app.mvvm_di.data.service.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor():HttpLoggingInterceptor{
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+    @Singleton
+    @Provides
+    fun provideOkHttp(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ):OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ):Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit):ApiService{
+        return retrofit.create()
+    }
+    @Singleton
+    @Provides
+    fun provideApiHelper(apiService: ApiService):ApiHelper{
+        return ApiHelper(apiService)
+    }
+}
